0c93e022feb36a93b6b64b70c6d7448d7a932cf9
rafay99-epic <99marafay@gmail.com>
2022-03-06 23:37:06 +0500

    adding variables

--- Manager-website/controllers/auth.js
+++ Manager-website/controllers/auth.js
@@ -11,5 +11,13 @@
 
 exports.register =(req,res) => {
     console.log(req.body);
+
+    const user_name = req.body.user_name;
+    const user_email = req.body.user_email;
+    const user_password = req.body.user_password;
+    const user_DOB= req.body.DOB;
+
+
+
     res.send("Form is Submitted");
 }
