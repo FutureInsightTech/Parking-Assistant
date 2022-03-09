6a259bbf1ad9dfc602a9df5bd1112a808520141e
KHILJI <sameermalik0478@gmail.com>
2022-03-09 14:18:22 +0500

    Update managerlogin.js
    Login Feature Updated, Minor bug fixed.

--- Manager-website/controllers/managerlogin.js
+++ Manager-website/controllers/managerlogin.js
@@ -1,30 +1,34 @@
 const con = require("../database/connection");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 const sessionfile = require("../session/session");
 
-exports.register =(req,res,next) => {
+exports.register = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    // console.log("Her I am ");
-    const email = req.body.email;
-    const password = req.body.password;
-    // console.log(req.body.email);
-    // console.log(req.body.password);
+  const sqlString = "SELECT password FROM manager WHERE email = ?";
 
-    var sql = "SELECT * from manager WHERE email = ?";
-
-    con.query(sql,[email], function(err, result,fields) {
-        if(err) {
-            console.log(err);
-        }
-        if (result.length && bcrypt.compareSync(password, result[0].password)) {
-            req.session.email = email;
-            console.log(req.session.email);
-            console.log("You are logged in successfully");
-            res.render('manager-dashboard',{
-                message: 'Welcome!!,'+ req.session.email
-            });
-            res.redirect("/manager-dashboard");
-        }
-    });
-
-}
+  con.query(sqlString, [email], async (err, results, fields) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    if (!results.length) {
+      console.log(" The Username is Invalid ");
+      res.redirect("/manager_login");
+      res.end();
+      return;
+    }
+    const isValid = await bcrypt.compare(password, results[0].password);
+    if (isValid) {
+      res.redirect("/manager-dashboard");
+      res.end();
+    } else {
+      console.log("The Password is Invalid");
+      res.redirect("/manager_login");
+      res.end();
+      return;
+    }
+  });
+};
