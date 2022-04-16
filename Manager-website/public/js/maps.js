701f63c691cc890967a5032090452b813d7b2624
Smalick0478 <sameermalik0478@gmail.com>
2022-04-16 18:02:13 +0500

    Maps File Updated.

--- Manager-website/public/js/maps.js
+++ Manager-website/public/js/maps.js
@@ -8,6 +8,34 @@ function initMap() {
   infoWindow = new google.maps.InfoWindow();
 
   const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Click To Get Sensors Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Sensors found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+          console.log(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
