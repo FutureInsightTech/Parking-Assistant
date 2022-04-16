a6aba8441159b9633cfb2e05c216051d70b99c71
Smalick0478 <sameermalik0478@gmail.com>
2022-04-16 18:01:48 +0500

    Maps File Updated

--- Manager-website/public/js/maps.js
+++ Manager-website/public/js/maps.js
@@ -1,5 +1,15 @@
 let map, infoWindow;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 30.0, lng: 70.0 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+}
+
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
