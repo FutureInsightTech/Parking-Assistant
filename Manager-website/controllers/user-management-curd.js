4c0f72cb824eab71b651ee61d1040b0b975f3777
Smalick0478 <sameermalik0478@gmail.com>
2022-04-16 16:45:49 +0500

    User Management CRUD Updated

--- Manager-website/controllers/user-management-curd.js
+++ Manager-website/controllers/user-management-curd.js
@@ -8,43 +8,39 @@
 //  #                                                                             #
 //  ###############################################################################
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 //This the env where the important passord and file will be kept
 const dotenv = require("dotenv");
 
 //Getting the .ev file
-dotenv.config({path:  './.env'});
+dotenv.config({ path: "../.env" });
 
 // Connection Pool
 let connection = mysql.createConnection({
-    connectionLimit: 10000,
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+  connectionLimit: 10000,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 });
 
 // View Users
 exports.view = (req, res) => {
   // User the connection
-  connection.query('SELECT * FROM user', (err, rows) => {
-    connection.release();
-    // When done with the connection, release it
+  connection.query("SELECT * FROM user", (err, rows) => {
     if (!err) {
-      res.render('/view-update-delete-user-profile', { rows });
+      res.render("view-update-delete-user-profile", { rows });
     } else {
       console.log(err);
     }
-    console.log('The data from user table: \n', rows);
   });
-}
+};
+
 // //Expess Libiary for server run
 // const express = require("express");
 
 // //mysql libiary
 // const mysql = require("mysql");
-
-
 
 // const pool= mysql.createPool({
 //     host: process.env.DATABASE_HOST,
@@ -58,10 +54,10 @@ exports.view = (req, res) => {
 //     pool.getConnection((err, connection) => {
 //         if(err) throw err;
 //         console.log('Connection ID:', connection.threadId);
-//         //User the connection 
+//         //User the connection
 //         connection.query("SELECT * FROM `user`", (err, rows) => {
 //             connection.release();
-            
+
 //             if(!err){
 //                 res.render('view-update-delete-user-profile', { rows });
 //             }
@@ -75,10 +71,10 @@ exports.view = (req, res) => {
 // }
 //To view the user profile from the database
 // exports.view =(req, res) =>{
-    
+
 //         connection.query("SELECT * FROM `user`", (err,rows) =>{
 
-              //when done the connection will be releases
+//when done the connection will be releases
 //             connection.release();
 //             if(!err){
 //                 res.render("view-update-delete-user-profile", { rows });
@@ -86,14 +82,11 @@ exports.view = (req, res) => {
 //             else{
 //                 console.log(err + "There is a problem");
 //             }
-    
-//             console.log("The data from user table: \n "+rows);
-    
-//         })
-        
 
+//             console.log("The data from user table: \n "+rows);
+
+//         })
 
 //     });
 
-    
 // }
