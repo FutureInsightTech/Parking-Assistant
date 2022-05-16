994f098792795376b27c193cc3d377a8afa89c85
Abdul Rafay <99marafay@gmail.com>
2022-05-16 14:08:09 +0500

    Adding More Pages

--- Manager-website/routes/auth.js
+++ Manager-website/routes/auth.js
@@ -22,6 +22,11 @@ const reservationRate = require("../controllers/rent_rate")
 const updateRentRate = require("../controllers/update_rate")
 //getting the user login controller from controller page
 const userlogin = require("../controllers/userlogin")
+//Calling the reverseparking controller
+const reverseparking= require("../controllers/reverseparking")
+
+//Enter Data into the Database
+//Manager Side
 
 //getting the router for the webpages.
 const router = express.Router();
@@ -36,19 +41,38 @@ router.post("/manager_login",managerloginController.register);
 router.post("/rent_rate",reservationRate.register);
 //this will redirect to the update rent page
 router.post("/update_rate",updateRentRate.register);
+
+
+//View Data From the Database
+//Manager Side
+
 //redirecting to the usermanagerment curd oprations
 const usermanagementController = require("../controllers/user-management-curd")
-
 //this will redirect the user manager controller in the controller folder
 router.get("/view-update-delete-user-profile",usermanagementController.view);
 //View Suggestions in the suggestion pages
 const suggestion_view = require("../controllers/suggestion_view")
 router.get("/view-suggestions",suggestion_view.view);
 
-
+//Enter Data into the Database
 //User Section: 
+
 //This will redirect to the user login controller
 router.post("/user-login",userlogin.register);
+//this will redirect to the reverseparking controller
+router.post("/reverseparking",reverseparking.register);
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Exporting this module to run
 module.exports = router;
